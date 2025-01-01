11cada79a8c6a1e025252ef3998befa8925e4f92
Alex Gan <a3gan@uwaterloo.ca>
2025-01-01 18:15:34 -0500

    improvements

--- merge-pdf.py
+++ merge-pdf.py
@@ -6,6 +6,9 @@ parser = argparse.ArgumentParser("Merge PDF")
 parser.add_argument("pdf_files", nargs="*")
 args = parser.parse_args()
 
+if len(args.pdf_files) < 2:
+    raise RuntimeError("Input at least 2 pdf files")
+
 for pdf in args.pdf_files:
     full_file_name = str(pdf)
     extension = pdf[pdf.rfind('.'):]
@@ -20,5 +23,9 @@ merger = PdfWriter()
 for pdf in args.pdf_files:
     merger.append(pdf)
 
-merger.write("merged-pdf.pdf")
+merged_pdf_file_name = "merged-pdf.pdf"
+
+merger.write(merged_pdf_file_name)
 merger.close()
+
+print("Created " + merged_pdf_file_name)
